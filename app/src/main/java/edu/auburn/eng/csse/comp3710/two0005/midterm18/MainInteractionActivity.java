685093cd845b69358c9923f6c6992a9f5f4f3f5b
Tanner Oakes <tanner.w.oakes@gmail.com>
2018-03-14 17:42:21 -0500

    First commit

---
+++ app/src/main/java/edu/auburn/eng/csse/comp3710/two0005/midterm18/MainInteractionActivity.java
@@ -0,0 +1,9 @@
+package edu.auburn.eng.csse.comp3710.two0005.midterm18;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainInteractionActivity extends SingleFragmentActivity {
+
+    
+}
---
+++ app/src/main/java/edu/auburn/eng/csse/comp3710/two0005/midterm18/SingleFragmentActivity.java
@@ -0,0 +1,29 @@
+package edu.auburn.eng.csse.comp3710.two0005.midterm18;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by tanneroakes6 on 3/14/18.
+ */
+
+public abstract class SingleFragmentActivity extends AppCompatActivity {
+
+    protected abstract Fragment createFragment();
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_interaction);
+
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
+
+        if (fragment == null) {
+            fragment = createFragment();
+            fm.beginTransaction().add(R.id.fragment_container, fragment).commit();
+        }
+    }
+}
